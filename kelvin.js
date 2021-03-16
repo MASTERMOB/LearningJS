cd62a0b3db983540acb1267f7f12acf5cc9f963a
MASTERMOB <80793523+MASTERMOB@users.noreply.github.com>
2021-03-17 00:31:40 +0200

    task1 solve

---
+++ kelvin.js
@@ -0,0 +1,15 @@
+function convertor(valoare , tip){
+  let kelvinToCelsius = valoare-273;
+if(tip === "Fahrenheit"){
+let fahrenheit = fahrenheit = kelvinToCelsius * (9/5) + 32;
+  return "TEMPERATURE IS " + Math.floor(fahrenheit)  +" Farinhate" ;
+  }
+else if (tip === "Celsius")  {
+  
+  return "TEMPERATURE IS " + Math.floor(kelvinToCelsius) +" Celsius";
+  }
+}
+
+console.log(convertor(500.4, "Fahrenheit" ))
+
+console.log(convertor(2.4, "Celsius" ))
